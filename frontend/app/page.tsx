1c7cf2b4924f316249ca59a687fa8424e5fc58d4
Igor Chaves Donega <236210133+ichavesbr@users.noreply.github.com>
2026-09-13 18:12:35 +0900

    feat: add temporary fetch to API

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+export const dynamic = "force-dynamic" // evita pré-render estático no build
+
+export default async function Home() {
+  const res = await fetch(`${process.env.BACKEND_URL}/products`)
+  if (!res.ok) throw new Error("Erro ao buscar produtos")
+  const products = await res.json()
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <h1>WELCOME</h1>
+      <br />
+      <div>
+        <p>Dados do backend:</p>
+        {products.map(({ id, name, price }: { id: number; name: string; price: number }) => (
+          <ul key={id}>
+            <li>
+              {id} - {name} - {price}
+            </li>
+          </ul>
+        ))}
+      </div>
     </div>
   )
 }
